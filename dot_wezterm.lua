f600015dd0281610db64af8872101c2a36333dce
TC Tseng <tc_tseng@gss.com.tw>
2026-03-25 00:03:37 +0800

    Update dotfiles: tc-tseng

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -41,10 +41,6 @@ config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
 
-config.use_ime = true
-config.disable_default_key_bindings = false
-config.front_end = "Software"
-
 -- 設定 Leader Key (Alt + a)
 config.leader = { key = "a", mods = "ALT", timeout_milliseconds = 1000 }
 
