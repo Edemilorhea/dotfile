3ab34499ff1f0dd40ea38717b4c7a95eb346ae4a
TC Tseng <tc_tseng@gss.com.tw>
2026-07-02 11:18:48 +0800

    Update dotfiles: tc-tseng

--- dot_psmux/plugins/ppm/scripts/clean_plugins.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-# PPM - Remove plugins not declared in config
-$ErrorActionPreference = 'Continue'
-$PPM_ROOT = Split-Path -Parent $PSScriptRoot
-. "$PPM_ROOT\ppm.ps1"
-Remove-UnusedPlugins
