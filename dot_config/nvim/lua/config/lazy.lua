dc2cc5755625255677c6376f2f8403fa2164c9f0
TC Tseng <tc_tseng@gss.com.tw>
2026-08-08 02:25:56 +0800

    Update dotfiles: tc-tseng

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
         { import = "plugins.blink" }, -- blink.cmp 覆寫配置
         { import = "plugins.development" }, -- 開發工具
         { import = "plugins.formatting" }, -- 格式化設定 (conform.nvim)
+        { import = "plugins.persistence" }, -- session 儲存設定
         { import = "plugins.tools" }, -- 工具插件 (Telescope、浮動終端等)
         { import = "plugins.neovim-only" }, -- 純 Neovim 插件
         { import = "plugins.ui-restructured" }, -- UI 插件
